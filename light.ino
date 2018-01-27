e7819359b8b333754ddca67f4622ecc55b84f5e7
Dongjin, Choi <milkelf.choi@gmail.com>
2018-01-27 21:47:51 +0900

    fixed button error

--- light.ino
+++ light.ino
@@ -16,26 +16,34 @@ bool isDemo = false;
 bool byTimeFlag = false;
 
 //short dimmingState = 0;
-short delayCnt = 0;
 bool sunFlag = false; // true : sunrise, false sunset
 
-long changeTime = 0;
+unsigned long changeTime = 0;
+
+unsigned long pushSwtichTime = 0;
+unsigned long prevGetDTTime = 0;
+
+unsigned short g_interval = 0;
 
 const char *dayWeekData[7] = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+
+#define MAX_LIGHT 220 // max is 255
 
 #define RISE_HOUR 16
 #define RISE_MIN 1
 #define RISE_SEC 0
 
-#define SET_HOUR 23
-#define SET_MIN 1
+#define SET_HOUR 22
+#define SET_MIN 30
 #define SET_SEC 0
 
 //#define DIM_STEP 1
 //#define DIM_INTERVAL 3000
 
 #define DIM_STEP 1
-#define DIM_INTERVAL 10000
+#define DIM_INTERVAL 15000
+
+#define DEMO_DIM_INTERVAL 50
 
 #define BUFF_MAX 16
 #define DEBUG
@@ -58,74 +66,98 @@ void setup() {
 }
 
 void loop() {
-   dt = clock.getDateTime();
-   if (delayCnt == 0) {
-      digitalWrite(ledPin, LOW);
-   } else {
-      delayCnt --;
-   }
-   
+  if ( millis() - prevGetDTTime > 10000) {
+    Serial.println("get Time");
+    dt = clock.getDateTime();
+    Serial.println(clock.dateFormat("d-m-Y H:i:s", dt));
+    prevGetDTTime = millis();
+  }
+
   // put your main code here, to run repeatedly:  
   if ( LOW == digitalRead(swtPin)) { //switch push..
-    Serial.println(clock.dateFormat("d-m-Y H:i:s", dt));
     digitalWrite(ledPin, HIGH);
-    delayCnt = 3;  //led delayCnt
-    sunFlag = sunFlag ? false : true;
-    dimmingStep = 3;
-    isDemo = true;
-    /*
-    dimmingState++;
-    if (dimmingState % 5 == 0)
-    {
-      Serial.println("PWM : 0");
-      analogWrite(dimmingPin, 0);
-    } else if (dimmingState % 5 == 1) {
-      Serial.println("PWM : 25%");
-      analogWrite(dimmingPin, 63);
-    } else if (dimmingState % 5 == 2) {
-      Serial.println("PWM : 50%");
-      analogWrite(dimmingPin, 127);
-    } else if (dimmingState % 5 == 3) {
-      Serial.println("PWM : 75%");
-      analogWrite(dimmingPin, 190);
-    } else if (dimmingState % 5 == 4) {
-      Serial.println("PWM : 100%");
-      analogWrite(dimmingPin, 255);
+    if (pushSwtichTime == 0) {
+       pushSwtichTime = millis();
     }
-    */
+  } else {
+    digitalWrite(ledPin, LOW);
+    if (pushSwtichTime > 0) {
+      Serial.println(millis() - pushSwtichTime);
+      if (millis() - pushSwtichTime > 500) {
+        //start dimming.
+        if (byTimeFlag) {
+          g_interval = DEMO_DIM_INTERVAL;
+        } else {
+          sunFlag = !sunFlag;
+          g_interval = DEMO_DIM_INTERVAL;
+        }
+
+        isDemo = true;
+        byTimeFlag =  false;
+      }
+      pushSwtichTime = 0;
+    }
+      /*
+      dimmingState++;
+      if (dimmingState % 5 == 0)
+      {
+        Serial.println("PWM : 0");
+        analogWrite(dimmingPin, 0);
+      } else if (dimmingState % 5 == 1) {
+        Serial.println("PWM : 25%");
+        analogWrite(dimmingPin, 63);
+      } else if (dimmingState % 5 == 2) {
+        Serial.println("PWM : 50%");
+        analogWrite(dimmingPin, 127);
+      } else if (dimmingState % 5 == 3) {
+        Serial.println("PWM : 75%");
+        analogWrite(dimmingPin, 190);
+      } else if (dimmingState % 5 == 4) {
+        Serial.println("PWM : 100%");
+        analogWrite(dimmingPin, 255);
+      }
+      */
+    
   }
 
-  if (dt.hour == RISE_HOUR && dt.minute == RISE_MIN && dt.second == RISE_SEC ) {
-    Serial.println("SUNRISE");
-    sunFlag = true;
-    dimmingStep = DIM_STEP;
-    byTimeFlag =  true;
+  if (!isDemo) {
+     if (!sunFlag && dt.hour == RISE_HOUR && dt.minute == RISE_MIN && dt.second > RISE_SEC ) {
+        Serial.println("SUNRISE");
+        sunFlag = true;
+        byTimeFlag =  true;
+        g_interval = DIM_INTERVAL;
+     }
+  
+     if (sunFlag && dt.hour == SET_HOUR && dt.minute == SET_MIN && dt.second > SET_SEC ) {
+       Serial.println("SUNSET");
+       sunFlag = false;
+       byTimeFlag = true;
+       g_interval = DIM_INTERVAL;
+     }
   }
-
-  if (dt.hour == SET_HOUR && dt.minute == SET_MIN && dt.second == SET_SEC ) {
-    Serial.println("SUNSET");
-    sunFlag = false;
-    dimmingStep = DIM_STEP;
-    byTimeFlag = true;
-  }
-
+  
   char tBuff2[BUFF_MAX] = {0,};
   if (isDemo || byTimeFlag ) {
-    if (isDemo || millis() - changeTime > DIM_INTERVAL)
+    if (millis() - changeTime > g_interval)
     {
         if (sunFlag == true) {
-           if (dimmingVal < 255){
-               dimmingVal += dimmingStep;
+           if (dimmingVal < MAX_LIGHT){
+               dimmingVal += DIM_STEP;
            } else {
-              dimmingVal = 255;
+              dimmingVal = MAX_LIGHT;
               isDemo = false;
+              byTimeFlag = false;
            }
         } else {
            if (dimmingVal > 0){
-               dimmingVal -= dimmingStep;
+               dimmingVal -= DIM_STEP;
+               if (dimmingVal < 0 ) {
+                  dimmingVal = 0;
+               }
            } else {
               dimmingVal = 0;
               isDemo = false;
+              byTimeFlag = false;
            }
         }
     }
@@ -139,6 +171,10 @@ void loop() {
      analogWrite(dimmingPin, dimmingVal);
      changeTime = millis();
   }
-
-  delay(500);
+  
+  if (isDemo) {
+    delay(50);
+  } else {
+    delay(250);  
+  }
 }
